92c1ba728f4561c7f032d93d91803bbb89afafbb
chrandle <christianrandle042@gmail.com>
2020-03-20 15:45:00 -0500

    added group controller functions

--- src/main/java/com/revature/controllers/GroupController.java
+++ src/main/java/com/revature/controllers/GroupController.java
@@ -1,8 +1,6 @@
-/**
- * 
- */
 package com.revature.controllers;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,13 +15,66 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.revature.models.Group;
 import com.revature.models.User;
+import com.revature.services.GroupService;
 import com.revature.services.UserService;
+
 
 @RestController
 public class GroupController {
 
+	@Autowired
+	private GroupService gService;
 	
+	ObjectMapper mapper = new ObjectMapper();
 
+	
+	//New Group
+	@PostMapping("/group")
+	public ResponseEntity<String> newGroup(@RequestBody String object){
+		try {
+			Group g = mapper.readValue(object, Group.class);
+			gService.createGroup(g);
+			return ResponseEntity.ok().body("{ \"Group\": "+mapper.writeValueAsString(g)+"}");
+		} catch (JsonProcessingException e) {
+			//return exception message on failure
+			return ResponseEntity.ok().body(e.getMessage());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			return ResponseEntity.ok().body(e.getMessage());
+		}
+	}
+
+	//Get all groups
+	@GetMapping("/group")
+	public ResponseEntity<String> getAll(){
+		List<Group>  groups = gService.getAllGroups();
+		try {
+			return ResponseEntity.ok().body("{ \"Groups\": "+mapper.writeValueAsString(groups)+"}");
+		} catch (JsonProcessingException e) {
+			//return exception message on failure
+			return ResponseEntity.ok().body(e.getMessage());
+		}
+	
+	}
+	
+	//Get one group by id
+	@GetMapping("/group/{group_id}")
+	public ResponseEntity<String> getOne(@PathVariable int group_id){
+		Group g  = gService.getGroupById(group_id);
+		
+		try {
+			return ResponseEntity.ok().body( "{"+"\"Group\": "+mapper.writeValueAsString(g)+"}");
+		} catch (JsonProcessingException e) {
+			//return exception message on failure
+			return ResponseEntity.ok().body(e.getMessage());
+		}
+	}
 
 }
--- src/main/java/com/revature/models/Group.java
+++ src/main/java/com/revature/models/Group.java
@@ -18,6 +18,7 @@ import javax.persistence.Table;
 
 import org.springframework.stereotype.Component;
 
+
 @Component
 @Entity
 @Table(name="appGroup")
