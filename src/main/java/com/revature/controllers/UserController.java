eb301487beef0e1d1386691674ff4ff80bf0aeb7
chrandle <christianrandle042@gmail.com>
2020-03-19 23:54:20 -0500

    basic user mapping, updated to rest

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -3,97 +3,91 @@ package com.revature.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.models.User;
 import com.revature.services.UserService;
 
-@Controller
-@RequestMapping("/users")
+@RestController
 public class UserController {
 
 	@Autowired
 	private UserService uService;
-	
-	@PostMapping("/new_user")
-	@ResponseStatus(value=HttpStatus.CREATED)
-	@ResponseBody
-	public User registerUser(@RequestParam("spotify_id") String spotify_id) {
+
+	//New User
+	@PostMapping("/user")
+	public ResponseEntity<User> createUser(@RequestBody String spotify_id) {
 		User u = new User(spotify_id);
 		uService.createUser(u);
-		return u;
+		return ResponseEntity.ok().body(u);
+	}
+
+	//Get all users
+	@GetMapping("/user")
+	public ResponseEntity<List<User>>  getAllUsers(){
+		List<User>  users = uService.getAllUsers();
+		return ResponseEntity.ok().body(users);
 	}
 	
-	@GetMapping("/all_users")
-	@ResponseBody
-	public List<User> allUsers(){
-		return uService.getAllUsers();
+	//Get one user by ID
+	@GetMapping("/user/{appUser_id}")
+	public ResponseEntity<User> getUserById(@PathVariable("appUser_id") int id) {
+		User u =  uService.getUserById(id);
+		return ResponseEntity.ok().body(u);
 	}
 
-	@GetMapping("/user_by_id")
-	@ResponseBody
-	public User userbyID(@RequestParam("appUser_id") int id) {
-		return uService.getUserById(id);
+	//One user by Spotify id
+	@GetMapping("/user/spot/{spotify_id}")
+	public ResponseEntity<User>  getUserBySpotifyId(@PathVariable("spotify_id") String spotify_id) {
+		User u =  uService.getUserBySpotifyId(spotify_id);
+		return ResponseEntity.ok().body(u);
 	}
 
-
-	@GetMapping("/user_by_spotify")//nope
-	@ResponseBody
-	public User userbyspotID(@RequestParam("spotify_id") String spotify_id) {
-		return uService.getUserBySpotifyId(spotify_id);
+	//One users in a group
+	@GetMapping("/user/g/{group_id}")
+	public ResponseEntity<List<User>> getUsersByGroupId(@PathVariable("group_id") int groupId) {
+		List<User> users =  uService.getUsersByGroupId(groupId);
+		return ResponseEntity.ok().body(users);
 	}
 
-
-	@GetMapping("/users_by_group")
-	@ResponseBody
-	public List<User> groupUsers(@RequestParam("group_id") int groupId) {
-		return uService.getUsersByGroupId(groupId);
-	}
-
-
-	@GetMapping("/update_user")
-	@ResponseBody
-	public boolean  UserUpdate(@RequestParam("appUser_id") int appUser_id) {
+	//Update User
+	@PutMapping("/user/update/{appUser_id}")
+	public ResponseEntity<?>  updateUser(@PathVariable("appUser_id") int appUser_id) {
 		User u = uService.getUserById(appUser_id);
-		if (u == null) return false;
+		if (u == null) return ResponseEntity.badRequest().body(null);
 		uService.updateUser(u);
-		return true;
+		return ResponseEntity.ok().body("User updated!");
 	}
 	
 	//TODO: testing
-	@PostMapping("/user_to_group")
-	@ResponseBody
-	public boolean usertoGroup(@RequestParam("appUser_id") int appUser_id, 
-			@RequestParam("group_id") int groupId) {
+	@PutMapping("/user/{appUser_id}")
+	public ResponseEntity<?> addUserToGroup(@PathVariable("appUser_id") int appUser_id, @RequestBody int groupId) {
 			User u = uService.getUserById(appUser_id);
-			if (u == null) return false;
+			if (u == null) return ResponseEntity.badRequest().body(null);
 			uService.addUserToGroup(u, groupId);
-			return true;
+			return ResponseEntity.ok().body(null);
 	}
 	
-	@PostMapping("/remove_user")
-	@ResponseBody
-	public boolean deleteUser(@RequestParam("appUser_id") int appUser_id, 
-			@RequestParam("group_id") int groupId) {
+	//remove user from group
+	@DeleteMapping("/user")
+	public ResponseEntity<?> removeUserFromGroup(@PathVariable("appUser_id") int appUser_id, @RequestBody int groupId) {
 			User u = uService.getUserById(appUser_id);
-			if (u == null) return false;
+			if (u == null) return ResponseEntity.badRequest().body(null);
 			 uService.removeUserFromGroup( u,  groupId);
-			 return true;
+			 return ResponseEntity.ok().body(null);
 	}
 	
 
